54ffc3794d43663c4d87afab5968fbe3787e32a1
unknown <you@example.com>
2020-06-09 21:26:19 +0300

    aa

--- src/components/AnimalsContainer.js
+++ src/components/AnimalsContainer.js
@@ -12,8 +12,7 @@ const AnimalsContainer =props => {
         }, '_id  name kind sex age city author phone info'));
     }
    
-        return props.animals.map((animals, index) =>(
-            <div className="container" key={index} style={containerStyle}>
+        return <div className="container"  style={containerStyle}>
             <div className="col-xs-12 col-md-12 ">
             <div className="post" style={postStyle}>
             <div className="col-xs-5 col-sm-4 thumbnail-col" style={infoStyle}>
@@ -21,29 +20,29 @@ const AnimalsContainer =props => {
                 width="100%" height="100%"/>
             </div>
            <div className="col-xs-7 col-sm-3 info-col" style={infoStyle}>
-               <h3 className="postTitle" title="Мейси" style={titleStyle} > {animals.name}</h3>
+               <h3 className="postTitle" title="Мейси" style={titleStyle} > {props.animals.name}</h3>
                <div className="postData">
-                       <p className="pleft">Вид: {animals.kind} </p>
-                       <p className="pleft">Пол: {animals.sex} </p>
-                       <p className="pleft">Възраст: {animals.age} </p>
-                       <p className="pleft">Населено място: {animals.city} </p>
+                       <p className="pleft">Вид: {props.animals.kind} </p>
+                       <p className="pleft">Пол: {props.animals.sex} </p>
+                       <p className="pleft">Възраст: {props.animals.age} </p>
+                       <p className="pleft">Населено място: {props.animals.city} </p>
                </div>
                <div className="btnMore" style={btnMore}>
                <button className="btn info">Още</button>
                </div>
            </div>
            <div className="col-xs-12 col-sm-4 author-col" style={authorStyle}>
-               <p className="author"> <h4>{animals.author}</h4></p>
+               <p className="author"> <h4>{props.animals.author}</h4></p>
                <div className="authorInfo">
-                   <p className="contactInfo">Телефон: {animals.phone}</p>
-                   <p> Допълнителна информация:{animals.info}</p>
+                   <p className="contactInfo">Телефон: {props.animals.phone}</p>
+                   <p> Допълнителна информация:{props.animals.info}</p>
                </div>
                <button type="button" className="btn btn-danger mb-1" onClick={dispatchDeleteAnimal}>Изтрий</button>
            </div>
         </div>    
         </div>
         </div>
-        ));
+       
     
 }
 
--- src/network/graphql-client.js
+++ src/network/graphql-client.js
@@ -1,7 +1,7 @@
 import {ApolloClient} from 'apollo-client';
 import {createHttpLink} from 'apollo-link-http';
 import {InMemoryCache} from 'apollo-cache-inmemory';
-import constants from 'constants';
+import constants from '../constants';
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
